c4e84fa7e775469888941b1ea43715227fdcae5a
Pablo Cozzi <cozzipablo1@gmail.com>
2025-06-15 16:55:54 -0300

    TP  1

---
+++ 01 Estructuras Secuenciales/cozzi_pablo_tp1.py
@@ -0,0 +1,70 @@
+# 1
+# print ("Hola Mundo")
+
+# 2
+# nombre = input ("ingresa tu nombre: ")
+# print (f"Hola, {nombre}!")
+
+# 3
+# nombre = input ("Ingrese su nombre: ")
+# apellido = input ("Ingrese su apellido: ")
+# edad = input ("Ingrese su edad: ")
+# residencia = input ("Ingrese su lugar de residencia: ")
+# print (f"Mi nombre es {nombre} {apellido} tengo {edad} anios y vivo en {residencia}")
+
+# 4
+# radio = input("ingrese el radio del circulo: ")
+# radio = int(radio)
+# area = 3.14 * radio * radio
+# perimetro = 2 * 3.14 * radio
+# print (f"El area es: {area}")
+# print (f"El perimetro es: {perimetro}")
+
+# 5
+# segundos = input ("ingrese los segundos: ")
+# horas = int(segundos) / 3600
+# print (f"Los {segundos} segundos, corresponden a {horas} horas")
+
+# 6
+# numero = input ("Por favor ingrese el numero: ")
+# i = 0
+# numero = int (numero)
+# while i < 10:
+#     i += 1
+#     multiplicacion = i * numero
+#     print (f"{i} * {numero} = {multiplicacion}")
+
+# 7
+# numero_1 = int (input ("ingrese el primer numero: "))
+# numero_2 = int (input ("ingrese el segundo numero: "))
+# suma = numero_1 + numero_2
+# resta = numero_1 - numero_2
+# multiplicacion = numero_2 * numero_1
+# division = numero_1 / numero_2
+# print (f"La suma es = {suma}")
+# print (f"La resta es = {resta}")
+# print (f"La division es = {division}")
+# print (f"La multiplicacion es = {multiplicacion}")
+
+# 8
+# altura = float(input("ingrese su altura en metros: "))
+# peso = int(input("ingrese su peso en kilogramos: "))
+# imc = peso / (altura * altura)
+# print (f"Su indice de masa corporal es: {imc}")
+
+# 9
+# celcius = int(input("ingrese los grados celcius: "))
+# farent = 9/5 * celcius + 32
+# print (f"Los grados corresponden a {farent} grados Fahrenteit")
+
+# 10
+# num1 = int(input("ingrese un numero: "))
+# num2 = int(input("ingrese un numero: "))
+# num3 = int(input("ingrese un numero: "))
+# promedio = (num1 + num2 + num3 ) /3
+# print (f"El promedio es: {promedio}")
+
+    
+
+
+
